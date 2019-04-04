659339c15bfc5dc87676b58187f8c88a78c7ae51
Colm Woodlock <Colm Woodlock>
2019-04-04 19:03:15 +0100

    Displays rectangle on screen where tracking points are mapped inside

--- Project/Assets/Scripts/MeasureDepth.cs
+++ Project/Assets/Scripts/MeasureDepth.cs
@@ -10,7 +10,7 @@ public class MeasureDepth : MonoBehaviour {
 
     //Cutoff the screen to only read a certain amount of space
     //Slider to adjust sensitivity
-    [Range(0,1.0f)]
+    [Range(0, 1.0f)]
     public float mDepthSensitivity = 1;
     //See how far the kinect is away from the wall
     [Range(-10, 10f)]
@@ -35,20 +35,23 @@ public class MeasureDepth : MonoBehaviour {
     private List<ValidPoint> mValidPoints = null;
 
 
-    //Has reference to our kinect sensor
+    //Has reference to our kinect sensor and camera
     private KinectSensor mSensor = null;
+    private Camera mCamera = null;
 
     //Way to map depth data to colour points
     private CoordinateMapper mMapper = null;
 
+
     //Depth sensor creates image that is 512x424
     private readonly Vector2Int mDepthResolution = new Vector2Int(512, 424);
-
+    private Rect mRect;
 
     private void Awake()
     {
         mSensor = KinectSensor.GetDefault();
         mMapper = mSensor.CoordinateMapper;
+        mCamera = Camera.main;
 
         //Initialise array size
         int arraySize = mDepthResolution.x * mDepthResolution.y;
@@ -64,9 +67,17 @@ public class MeasureDepth : MonoBehaviour {
         {
             mValidPoints = DepthToColour();
 
+            mRect = CreateRect(mValidPoints);
+
             //Use depthtocolor to creat a texture
             mDeptTexture = CreateTexture(mValidPoints);
         }
+    }
+
+    //Test function to draw rectangle
+    private void OnGUI()
+    {
+        GUI.Box(mRect, "");
     }
 
     private List<ValidPoint> DepthToColour()
@@ -82,7 +93,7 @@ public class MeasureDepth : MonoBehaviour {
         List<ValidPoint> validPoints = new List<ValidPoint>();
 
         // Filter
-        for(int i = 0; i < mDepthResolution.x / 8; i++)
+        for (int i = 0; i < mDepthResolution.x / 8; i++)
         {
             //Divide by 8 to avoid index out of bounds
             for (int j = 0; j < mDepthResolution.y / 8; j++)
@@ -94,7 +105,7 @@ public class MeasureDepth : MonoBehaviour {
                 sampleIndex *= 8;
 
                 //Cut off tests
-                if(mCameraSpacePoints[sampleIndex].X < mLeftCutOff)
+                if (mCameraSpacePoints[sampleIndex].X < mLeftCutOff)
                 {
                     continue;
                 }
@@ -115,7 +126,7 @@ public class MeasureDepth : MonoBehaviour {
                 ValidPoint newPoint = new ValidPoint(mColorSpacePoints[sampleIndex], mCameraSpacePoints[sampleIndex].Z);
 
                 //Depth Test
-                if(mCameraSpacePoints[sampleIndex].Z >= mWallDepth)
+                if (mCameraSpacePoints[sampleIndex].Z >= mWallDepth)
                 {
                     newPoint.mWithinWallDepth = true;
                 }
@@ -134,7 +145,7 @@ public class MeasureDepth : MonoBehaviour {
         //Create new texture and make sure it is transparet by using Alpha8 and ensure it is lined up correctly
         Texture2D newTexture = new Texture2D(1920, 1080, TextureFormat.Alpha8, false);
 
-        for(int x = 0; x < 1920; x++)
+        for (int x = 0; x < 1920; x++)
         {
             for (int y = 0; y < 1080; y++)
             {
@@ -143,7 +154,7 @@ public class MeasureDepth : MonoBehaviour {
             }
         }
 
-        foreach(ValidPoint point in validPoints)
+        foreach (ValidPoint point in validPoints)
         {
             newTexture.SetPixel((int)point.colorSpace.X, (int)point.colorSpace.Y, Color.black);
         }
@@ -152,6 +163,98 @@ public class MeasureDepth : MonoBehaviour {
 
         return newTexture;
     }
+
+    #region Rect Creation
+    private Rect CreateRect(List<ValidPoint> points)
+    {
+        //If the points we pass in are 0 then we create an empty rect
+        if(points.Count == 0)
+        {
+            return new Rect();
+        }
+
+        // Get corners for the rect
+        Vector2 topLeft = GetTopLeft(points);
+        Vector2 bottomRight = GetBottomRight(points);
+
+        //Translate to viewport
+        Vector2 screenTopLeft = ScreenToCamera(topLeft);
+        Vector2 screenBottomRight = ScreenToCamera(bottomRight);
+
+
+        //Get dimensions of the rect (drawn from the top left)
+        int width = (int)(screenBottomRight.x - screenTopLeft.x);
+        int height = (int)(screenBottomRight.y - screenTopLeft.y);
+
+        //Create the rect
+        Vector2 size = new Vector2(width, height);
+        Rect rect = new Rect(screenTopLeft, size);
+
+
+        return rect;
+    }
+
+    //This will get the top left of the mapped area
+    private Vector2 GetTopLeft(List<ValidPoint> points)
+    {
+        //What is going to be the highest number
+        Vector2 topLeft = new Vector2(int.MaxValue, int.MaxValue);
+
+        foreach(ValidPoint point in points)
+        {
+            //LEft most x value
+            if(point.colorSpace.X < topLeft.x)
+            {
+                topLeft.x = point.colorSpace.X;
+            }
+            //Top most y value
+            if (point.colorSpace.Y < topLeft.y)
+            {
+                topLeft.y = point.colorSpace.Y;
+            }
+        }
+
+        return topLeft;
+    }
+
+    //This will get the bottom right of the game area
+    private Vector2 GetBottomRight(List<ValidPoint> points)
+    {
+        //What is going to be the highest number
+        Vector2 bottomRight = new Vector2(int.MinValue, int.MinValue);
+
+        foreach (ValidPoint point in points)
+        {
+            //Right most x value
+            if (point.colorSpace.X > bottomRight.x)
+            {
+                bottomRight.x = point.colorSpace.X;
+            }
+            //bottom most y value
+            if (point.colorSpace.Y > bottomRight.y)
+            {
+                bottomRight.y = point.colorSpace.Y;
+            }
+        }
+
+        return bottomRight;
+    }
+
+    //Converting two points topLeft + bottomRight and give proper location on sreen
+    private Vector2 ScreenToCamera(Vector2 screenPosition)
+    {
+        //Get the position of the screen
+        Vector2 normalisedScreen = new Vector2(Mathf.InverseLerp(0, 1920, screenPosition.x),
+            Mathf.InverseLerp(0, 1080, screenPosition.y));
+
+        //Convert normalisedScreen back to pixel position
+        Vector2 screenPoint = new Vector2(normalisedScreen.x * mCamera.pixelWidth, normalisedScreen.y * mCamera.scaledPixelHeight);
+
+        //return the screen positon
+        return screenPoint;
+    }
+
+    #endregion
 
 }
 
