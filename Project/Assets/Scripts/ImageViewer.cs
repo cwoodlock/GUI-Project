89ce9a68366691445448a525b78780140b6b5202
Colm Woodlock <Colm Woodlock>
2019-04-04 16:14:59 +0100

    Outputting raw camera footage from Kinect

---
+++ Project/Assets/Scripts/ImageViewer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageViewer : MonoBehaviour {
+
+    //This will give us the colour and depth data
+    public MultiSourceManager mMultiSource;
+
+    //What we will be using to output the raw image we are getting from the connect
+    public RawImage mRawImage; 
+	
+	// Update is called once per frame
+	void Update () {
+
+        //
+        mRawImage.texture = mMultiSource.GetColorTexture();
+	}
+}
